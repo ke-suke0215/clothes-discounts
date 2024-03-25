0b4cd4a2b66f1dcf9f26042b7a26fff1a09cf9ed
ke-suke0215 <keisuke.yohs.0215.1209@gmail.com>
2024-03-25 11:30:25 +0900

    フォームに入力した文字列で商品を検索する

--- app/components/layouts/header/Header.tsx
+++ app/components/layouts/header/Header.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import { AppBar, Box, TextField, Toolbar } from "@mui/material";
+import { AppBar, Box, Toolbar } from "@mui/material";
 import Link from "next/link";
 
 const Header = () => {
@@ -11,14 +11,6 @@ const Header = () => {
           <Link href="/">
             <Image src="/logo.png" alt="logo" width={140} height={40} />
           </Link>
-          {/* TODO: 検索アイコン入れたい */}
-          <TextField
-            id="outlined-basic"
-            label="商品名を入力してください"
-            variant="outlined"
-            size="small"
-            sx={{ marginLeft: "50px", width: "600px" }}
-          />
         </Toolbar>
       </AppBar>
       {/* AppBarの高さに合わせた空のToolbarを追加 */}
--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,64 @@
-import { Button, Grid, Paper, Container } from "@mui/material";
-import Image from "next/image";
+"use client";
+
+import { Grid, Container, TextField } from "@mui/material";
 import ItemCard from "./components/ItemCard";
+import { useState } from "react";
 
 async function getItems(name: String): Promise<Item[]> {
   const response = await fetch(`http://localhost:3000/api/items?name=${name}`, {
-    cache: "no-store",
+    method: "GET",
   });
   return await response.json();
 }
 
-export default async function Home() {
-  const items: Item[] = await getItems("セーター");
+export default function Home() {
+  const [items, setItems] = useState<Item[]>([]); // 状態を管理するための useState フック
+  const [searchText, setSearchText] = useState("");
+
+  const handleSearch = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setItems(await getItems(searchText));
+
+    // TODO: 検索結果がなかったときに何かしらのメッセージを表示したい
+  };
 
   return (
     <div>
       <Container maxWidth="lg" sx={{ mt: 3, mb: 6 }}>
-        <Grid container spacing={4} justifyContent="flex-start">
-          {items.map((item: Item) => (
-            <Grid key={item.id} item>
-              <ItemCard item={item} />
-            </Grid>
-          ))}
-        </Grid>
+        <form onSubmit={handleSearch}>
+          <TextField
+            id="outlined-basic"
+            label="商品名を入力"
+            variant="outlined"
+            size="small"
+            value={searchText}
+            sx={{ width: "60%", mb: 5 }}
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+        </form>
+        {/* TODO: 検索中のときにスケルトンを表示したい */}
+        {items.length === 0 ? (
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "400px",
+              color: "gray",
+              fontSize: "2rem",
+            }}
+          >
+            商品を検索してください
+          </div>
+        ) : (
+          <Grid container spacing={4} justifyContent="flex-start">
+            {items.map((item: Item) => (
+              <Grid key={item.id} item>
+                <ItemCard item={item} />
+              </Grid>
+            ))}
+          </Grid>
+        )}
       </Container>
     </div>
   );
