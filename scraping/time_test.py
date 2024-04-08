e8aa25fa548074120b306232d1eb82a93a48e80a
ke-suke0215 <keisuke.yohs.0215.1209@gmail.com>
2024-04-08 12:25:32 +0900

    chore fix

--- scraping/time_test.py
+++ scraping/time_test.py
@@ -4,7 +4,7 @@ now = datetime.datetime.now()
 print("現在時刻: ")
 print(now)
 
-# 現在から9時間前
-nine_hours_ago = now - datetime.timedelta(hours=9)
+# 現在から9時間後（日本時間）
+nine_hours_later = now + datetime.timedelta(hours=9)
 print("9時間前: ")
-print(nine_hours_ago)
+print(nine_hours_later)
