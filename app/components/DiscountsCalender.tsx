5afee6ef32cd7e8d6b9622c264da087cdcf20da1
ke-suke0215 <keisuke.yohs.0215.1209@gmail.com>
2024-04-17 18:44:45 +0900

    カレンダーコンポーネントに関数の雛形とTODO記載

--- app/components/DiscountsCalender.tsx
+++ app/components/DiscountsCalender.tsx
@@ -1,9 +1,80 @@
 import React from 'react'
+import dayjs, { Dayjs } from 'dayjs';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { PickersDay, PickersDayProps } from '@mui/x-date-pickers/PickersDay';
+import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+import { DayCalendarSkeleton } from '@mui/x-date-pickers/DayCalendarSkeleton';
+
+/**
+ * カレンダーの日付を表示する.
+ */
+function ServerDay(
+  props: PickersDayProps<Dayjs> & { highlightedDays?: number[] }
+) {
+  const { highlightedDays = [], day, outsideCurrentMonth, ...other } = props;
+
+  const isSelected =
+    !props.outsideCurrentMonth &&
+    highlightedDays.indexOf(props.day.date()) >= 0;
+
+  return (
+    <PickersDay
+      {...other}
+      outsideCurrentMonth={outsideCurrentMonth}
+      day={day}
+      selected={isSelected}
+    />
+  );
+}
+
+/**
+ * APIから日付データを取得する.
+ */
+const fetchDates = async (itemId: number) => {
+  // APIからデータ取得
+  return []
+}
 
 const DiscountsCalender = ({ itemId }: { itemId: number }) => {
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [highlightedDays, setHighlightedDays] = React.useState([1, 2, 15]);
+
+  // TODO: APIから日付データを取得する
+  // TODO: ハイライト日付に値をいれる
+
+  /**
+   * 月を変更したときの処理.
+   */
+  const handleMonthChange = (date: Dayjs) => {
+    console.log("月を変更：" + date.format('YYYY-MM'));
+    console.log("年：" + date.year());
+    console.log("月：" + (date.month() + 1));
+
+    setIsLoading(true);
+    setHighlightedDays([]);
+    // TODO: ハイライト日付に変更後の年月の値を入れる
+    setHighlightedDays([1, 2, 15]);
+    setIsLoading(false);
+  };
+
   
   return (
-    <div>DiscountsCalender{itemId}</div>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DateCalendar
+        loading={isLoading}
+        onMonthChange={handleMonthChange}
+        renderLoading={() => <DayCalendarSkeleton />}
+        slots={{
+          day: ServerDay,
+        }}
+        slotProps={{
+          day: {
+            highlightedDays,
+          } as any,
+        }}
+      />
+    </LocalizationProvider>
   )
 }
 
